4453099c7d71d7e82a1e53cafab07496e3c30eb4
arhammusheer <arhammusheer@gmail.com>
2022-10-26 06:04:30 -0400

    BRUH

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { APIURL } from "./urls";
 
-export const getUser = async (token: string) => {
+export const getUser = async () => {
   const response = await axios.get(`${APIURL}/user`);
   return response.data;
 };

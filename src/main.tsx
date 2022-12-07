204fcd3e68fcfe53052f44c01541153df57b9cbe
arhammusheer <arhammusheer@gmail.com>
2022-12-07 07:22:11 -0500

    BRUH

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import {
-  BrowserRouter,
-  createBrowserRouter,
-  redirect,
-  Routes,
+  BrowserRouter
 } from "react-router-dom";
 import "./index.css";
-import Chat from "./pages/Chat/Chat";
-import Chatbox from "./pages/Chat/Chatbox/Chatbox";
-import Introduction from "./pages/Chat/Introduction";
-import Landing from "./pages/Landing";
 import theme from "./utils/theme";
 
 import { Provider } from "react-redux";
-import { getUser } from "./apis/user";
-import LoginWithPassword from "./pages/auth/LoginWithPassword";
-import _404 from "./pages/errors/_404";
 import store from "./redux/store";
 import Routing from "./Routes";
 
